a237e4e3011e121c669f7c23197b523107c3da27
n0vad3v <n0vad3v@riseup.net>
2020-01-08 15:11:07 +0800

    Bump version to 0.0.2. Fix on italic optimization. Update setup.py.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="tekorrect",
-    version="0.0.1",
+    version="0.0.2",
     author="Nova Kwok",
     author_email="noc@nova.moe",
     description="用於給已經完成的 Markdown 遍寫的文檔進行優化，它會將壹些非標準，或不推薦的排版方式，進行自動格式化、標準化。",
@@ -13,8 +13,19 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/n0vad3v/Tekorrect",
     packages=setuptools.find_packages(),
+    keywords='pangu text-spacing spacing text typesetting readability chinese japanese korean obsessive-compulsive-disorder ocd paranoia',
     classifiers=[
         "Programming Language :: Python :: 3",
+        'Natural Language :: Chinese (Traditional)',
+        'Natural Language :: Chinese (Simplified)',
+        'Natural Language :: Japanese',
+        'Natural Language :: Korean',
+        'Topic :: Education',
+        'Topic :: Software Development :: Internationalization',
+        'Topic :: Text Processing',
+        'Topic :: Text Processing :: General',
+        'Topic :: Text Processing :: Linguistic',
+        'Topic :: Utilities',
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
--- tekorrect/tekorrect.py
+++ tekorrect/tekorrect.py
@@ -27,13 +27,15 @@ def call_pangu(name):
     new_file_content = re.sub(r'\~\~(\s)?(.*?)(\s)?\~\~',r'~~\2~~',new_file_content)
     # Fix Markdown bold(em) symbol
     new_file_content = re.sub(r'\*\*(\s)?(.*?)(\s)?\*\*',r'**\2**',new_file_content)
+    # Fix Markdown italic symbol
+    new_file_content = re.sub(r'\*(\s)?(.*?)(\s)?\*',r'*\2*',new_file_content)
     # OverWrite the original files
     with open(name, 'w', encoding='utf-8') as f:
         f.write(new_file_content)
 
 
 def exe_main():
-    parser = OptionParser()
+    parser = OptionParser(version="%prog 0.0.2")
     parser.add_option("-p", "--path", dest="path",
                       help="parse a directory", metavar="DIR")
     parser.add_option("-f", "--file", dest="file",
